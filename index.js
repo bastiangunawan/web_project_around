59416a71c73dde3646bcc88fe7b75d9c39aa9aeb
BastianGunawan <127901952+bastiangunawan@users.noreply.github.com>
2023-05-25 19:05:31 +0700

    bastian-gunawan

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-let content = document.querySelector('.content');
-let love = content.querySelectorAll('.photo-grid__cards-love');
+/*const love = document.querySelector('.photo-grid__cards-love');*/
+const love1 = document.querySelector('#love-1').onclick = function() {myFunction1()}
+const love2 = document.querySelector('#love-2').onclick = function() {myFunction2()}
+const love3 = document.querySelector('#love-3').onclick = function() {myFunction3()}
+const love4 = document.querySelector('#love-4').onclick = function() {myFunction4()}
+const love5 = document.querySelector('#love-5').onclick = function() {myFunction5()}
+const love6 = document.querySelector('#love-6').onclick = function() {myFunction6()}
 
-document.getElementById('love').onclick = function() {myFunction()}
 
-function myFunction() {
-    document.getElementById('love').src='./images/heart_active.svg';
+function myFunction1() {
+    document.querySelector('#love-1').src='./images/heart_active.svg';
+}
+
+function myFunction2() {
+    document.querySelector('#love-2').src='./images/heart_active.svg';
+}
+
+function myFunction3() {
+    document.querySelector('#love-3').src='./images/heart_active.svg';
+}
+
+function myFunction4() {
+    document.querySelector('#love-4').src='./images/heart_active.svg';
+}
+
+function myFunction5() {
+    document.querySelector('#love-5').src='./images/heart_active.svg';
+}
+
+function myFunction6() {
+    document.querySelector('#love-6').src='./images/heart_active.svg';
 }
