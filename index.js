493cd00c48730a204d15c1a805d3d8b13ef2cbe2
BastianGunawan <127901952+bastiangunawan@users.noreply.github.com>
2023-06-06 22:48:21 +0700

    bastian-gunawan

--- index.js
+++ index.js
@@ -5,14 +5,8 @@ const close = document.querySelector('.pop-up__close');
 const save = document.querySelector('.pop-up__save');
 const names = document.querySelector('.profile__info-names');
 const jobs = document.querySelector('.profile__info-jobs')
-const input = document.querySelector('input[id="names"]');
-const input2 = document.querySelector('input[id="jobs"]');
-const love1 = document.querySelector('#love-1');
-const love2 = document.querySelector('#love-2');
-const love3 = document.querySelector('#love-3');
-const love4 = document.querySelector('#love-4');
-const love5 = document.querySelector('#love-5');
-const love6 = document.querySelector('#love-6');
+const inputNames = document.querySelector('input[id="names"]');
+const inputJobs = document.querySelector('input[id="jobs"]');
 
 edit.addEventListener('click', () => {
     pop.classList.add('pop-up--active');
@@ -23,39 +17,70 @@ close.addEventListener('click', () => {
     body.classList.remove('body--active');
 });
 save.addEventListener('click', () => {
-    names.innerHTML = input.value;
-    jobs.innerHTML = input2.value;
+    names.innerHTML = inputNames.value;
+    jobs.innerHTML = inputJobs.value;
     pop.classList.remove('pop-up--active');
     body.classList.remove('body--active');
 });
 
-love1.addEventListener('click', myFunction1);
-love2.addEventListener('click', myFunction2);
-love3.addEventListener('click', myFunction3);
-love4.addEventListener('click', myFunction4);
-love5.addEventListener('click', myFunction5);
-love6.addEventListener('click', myFunction6);
+const template = document.querySelector('template');
+const photoGrid = document.querySelector('.photo-grid');
 
-function myFunction1() {
-    document.querySelector('#love-1').src='./images/heart_active.svg';
+const data = [
+    {
+        image: './images/Kaldera-Prapat-1.jpg',
+        title: 'Kaldera Parapat',
+        love: './images/heart.svg',
+    },
+
+    {
+        image: './images/Kaldera-Prapat-2.jpg',
+        title: 'Kaldera Parapat',
+        love: './images/heart.svg',
+    },
+
+    {
+        image: './images/Kaldera-Prapat-3.jpg',
+        title: 'Kaldera Parapat',
+        love: './images/heart.svg',
+    },
+
+    {
+        image: './images/Tara-Bunga.jpg',
+        title: 'Taram Bunga',
+        love: './images/heart.svg',
+    },
+
+    {
+        image: './images/Hotel-Niagara-Prapat.jpg',
+        title: 'Hotel Niagara Parapat',
+        love: './images/heart.svg',
+    },
+
+    {
+        image: './images/Hotel-Niagara-Prapat-View.jpg',
+        title: 'Hotel Niagara Parapat',
+        love: './images/heart.svg',
+    },
+]
+
+function loveActive() {
+    this.classList.add('photo-grid__cards-love--active')
 }
 
-function myFunction2() {
-    document.querySelector('#love-2').src='./images/heart_active.svg';
-}
+for (let i = 0; i < 6; i++) {
+    const clone = template.content.cloneNode(true);
 
-function myFunction3() {
-    document.querySelector('#love-3').src='./images/heart_active.svg';
-}
+    const gridCards = clone.querySelector('.photo-grid__cards');
+    const gridImages = clone.querySelector('.photo-grid__cards-item');
+    const gridText = clone.querySelector('.photo-grid__cards-text');
+    const gridLove = clone.querySelector('.photo-grid__cards-love');
 
-function myFunction4() {
-    document.querySelector('#love-4').src='./images/heart_active.svg';
-}
+    gridImages.src = data[i].image;
+    gridText.innerHTML = data[i].title;
+    gridLove.src = data[i].image;
 
-function myFunction5() {
-    document.querySelector('#love-5').src='./images/heart_active.svg';
-}
+    gridLove.addEventListener('click', loveActive);
 
-function myFunction6() {
-    document.querySelector('#love-6').src='./images/heart_active.svg';
+    photoGrid.appendChild(clone);
 }
